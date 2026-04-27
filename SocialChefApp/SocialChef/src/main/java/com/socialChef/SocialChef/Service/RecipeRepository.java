98739978fea10242c8d701db08345fcb84207f0e
Dennali Grissom <dennaligrissom7@gmail.com>
2026-04-26 20:01:28 -0400

    Upgrade to version 3.3.7

--- SocialChefApp/SocialChef/src/main/java/com/socialChef/SocialChef/Service/RecipeRepository.java
+++ SocialChefApp/SocialChef/src/main/java/com/socialChef/SocialChef/Service/RecipeRepository.java
@@ -1,6 +1,5 @@
 package com.socialChef.SocialChef.Service;
 
-import org.springframework.data.repository.CrudRepository;
 
-public interface RecipeRepository extends CrudRepository<Integer, String> {
+public interface RecipeRepository {
 }
